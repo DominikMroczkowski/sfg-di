83da871b9dbdf2ae96b9069b5337dcd394516757
Dominik <dominikjanmroczkowski@gmail.com>
2021-12-03 10:14:25 +0100

    Add example of getting beans with context

--- src/main/java/dominikmroczkowski/sfgdi/SfgDiApplication.java
+++ src/main/java/dominikmroczkowski/sfgdi/SfgDiApplication.java
@@ -2,12 +2,18 @@ package dominikmroczkowski.sfgdi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import dominikmroczkowski.sfgdi.controllers.MyController;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+		ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
+		MyController controller = (MyController) context.getBean("myController");
+
+		controller.sayHello();
 	}
 
 }
